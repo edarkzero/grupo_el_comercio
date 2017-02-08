396cdecda1646c1502f7a292272ea92902cf1094
edarkzero <edgarcardona87@gmail.com>
2017-02-08 16:04:12 -0500

    Primer commit; Proceso de numeros (validaciones y visualizacion) Hojas de estilo Javascripts Inclusion de JQuery TODO: animacion

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# grupo_el_comercio
-Prueba para puesto de frontend
+# Prueba Online - Grupo El Comercio
+* Prueba para puesto de programador frontend
+* Se usa el API para javascript JQuery para facilitar manejo de los eventos del DOM de elementos
+* Hojas de estilos almacenadas en carpeta "css/"
+* Javascripts almacenados en carpeta "js/"
---
+++ js/main.js
@@ -0,0 +1,80 @@
+//Variable global para llevar control del numero de numeros visibles
+var number_count = 1;
+
+//Cuando esta listo el DOM de elementos
+$(function() {
+
+    //Manejo de eventos de boton "mas numeros" el cual agregara mas numeros en la interfaz
+    $('#btn-more').click(function(e){
+        addNumberInput();
+    });
+
+    //Manejo de cambios en INPUT de numeros
+    $(document).on('keyup change','.number-input',function (e) {
+        checkNumbers($(this));
+    })
+
+});
+
+//Funcion para añadir plantilla para un nuevo numero
+function addNumberInput()
+{
+    var template_begin = '<input class="number-input" type="number" name="number[]" id="input-';
+    var template_last = '" min="0">';
+
+    //Añade al final del elemento seleccionado la plantilla con un ID unico basado en la variable number_count
+    $('#dynamic-number-wrapper').append(template_begin + number_count + template_last);
+
+    //actualiza contandor de numeros
+    number_count++;
+}
+
+//Funcion para verificar que los numeros no estan repetidos, recibe el elemento que inicio el evento
+function checkNumbers($elem)
+{
+    var numbers = [];
+    var $numberInput = $('.number-input');
+
+    //Añadiendo numeros a un array para procesarlos
+    $numberInput.each(function (i) {
+        var valToComp = $(this).val();
+        var duplicated = false;
+
+        //Verificando repetidos
+        $numberInput.each(function (j) {
+
+            //Si los indices son distintos y el valor a comparar es igual al actual, existe un duplicado
+            if(i != j && valToComp == $(this).val())
+            {
+                console.log('Repetido i:'+i+', j:'+j);
+                duplicated = true;
+            }
+        });
+
+        //Deplicado encontrado
+        if(duplicated)
+            $elem.val('');
+
+        //Sin duplicados
+        if($(this).val() != '')
+            numbers.push($(this).val());
+    });
+
+    //Limpiando resultados anteriores
+    $('#original-wrapper,#result-wrapper').html('');
+
+    ///Array original
+    $.each(numbers,function (index, value) {
+        var $wrapper = $('#original-wrapper');
+        if(index == 0) $wrapper.append('<h5>Original</h5>');
+        $wrapper.append('<b>'+value+',</b>');
+    });
+
+    //Array ordenado
+    numbers.sort();
+    $.each(numbers,function (index, value) {
+        var $wrapper = $('#result-wrapper');
+        if(index == 0) $wrapper.append('<h5>Ordenado</h5>');
+        $wrapper.append('<b>'+value+',</b>');
+    });
+}
